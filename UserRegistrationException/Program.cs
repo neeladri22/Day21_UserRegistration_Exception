989108c3966534095e19e7716db301e5e4a7bc67
unknown <neeladri22@gmail.com>
2022-07-10 13:48:26 +0530

    Welcome to the User Registration problem Using Exception

---
+++ UserRegistrationException/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the User Registration problem using Exception");
+        }
+    }
+}
